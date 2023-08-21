b01ece8b02ef3a556ce3b8d70bd8300f138ec05e
Jonathan Guzmán <jogule@gmail.com>
2023-08-21 17:00:00 -0500

    setup workflows

--- terraform-up.bat
+++ terraform-up.bat
@@ -1,6 +1,31 @@
 terraform fmt
 terraform validate
 terraform apply -auto-approve
-terraform import azapi_resource.scm-policy "/subscriptions/baa4da18-4abd-4ed6-b3ed-1bffa64e4b6b/resourceGroups/myapp-rg/providers/Microsoft.Web/sites/myapp-jonguz-xyz-webapp/basicPublishingCredentialsPolicies/scm"
-terraform import azapi_resource.ftp-policy "/subscriptions/baa4da18-4abd-4ed6-b3ed-1bffa64e4b6b/resourceGroups/myapp-rg/providers/Microsoft.Web/sites/myapp-jonguz-xyz-webapp/basicPublishingCredentialsPolicies/ftp"
-terraform apply -auto-approve
+
+git mv main_myapp-jonguz-xyz-webapp.yml PRD.yml
+git mv main_myapp-jonguz-xyz-webapp(qa).yml QAS.yml
+git mv main_myapp-jonguz-xyz-webapp(staging).yml STG.yml
+
+gh workflow list -R https://github.com/jogule/myapp -a
+#Azure App Service - myapp-jonguz-xyz-webapp(qa), Build and deploy DotnetCore app          active             66903386
+#Azure App Service - myapp-jonguz-xyz-webapp(staging), Build and deploy DotnetCore app     active             66886016
+#Azure App Service - myapp-jonguz-xyz-webapp(Production), Build and deploy DotnetCore app  disabled_manually  66800094
+
+#edit PRD.yml
+#name: PRD
+#gh workflow disable 66800094 -R https://github.com/jogule/myapp
+
+#edit STG.yml
+#name: STG
+
+#edit QAS.yml
+#name: QAS
+#on:
+# push:
+#   branches:
+#     - feat/*
+
+git add .
+git commit -m "setup workflows"
+git pull origin main
+git push origin HEAD
